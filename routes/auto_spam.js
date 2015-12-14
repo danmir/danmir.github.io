f4dc13e243774655dc5acfe0d9ef27fbe9fad152
danmir <danmir@me.com>
2015-12-14 20:43:19 +0500

    Comments

--- routes/auto_spam.js
+++ routes/auto_spam.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var allVisits = require('../get_statistic.js');
-var pg = require('pg');
 //var conString = "postgres://danmir:@localhost/visits";
 var query = require('pg-query');
 query.connectionParameters = process.env.DATABASE_URL;
 
-
+// Проверяем частоту обновлений страницы
+// Записываем визиты пользователей
 router.use(function (req, res, next) {
     // Получаем настоящий адрес, внутри heroku
     var realIpAddr = req.headers["x-forwarded-for"];
@@ -83,83 +83,5 @@ router.use(function (req, res, next) {
         }
     });
 });
-
-//// Определение быстрых обращений к серверу
-//router.use(function(req, res, next) {
-//    pg.connect(conString, function(err, client, done) {
-//        if(err) {
-//            return console.error('could not connect to postgres', err);
-//        }
-//        client.query("SELECT ip, time from visits WHERE ip=$1::text", [req.ip], function(err, result) {
-//            if(err) {
-//                return console.error('error running query', err);
-//            }
-//            if (!result.rowCount) {
-//                //console.log('Новый пользователь');
-//                client.query("INSERT INTO visits (time, ip, num) VALUES (extract(epoch from now()),'" + req.ip + "', 1)", function(err, result) {
-//                    if(err) {
-//                        return console.error('error running query', err);
-//                    }
-//                    // Запишем еще время этого визита
-//                    client.query("INSERT INTO visits_time (ip, time) VALUES ($1::text, CURRENT_DATE)", [req.ip], function(err, result) {
-//                        if(err) {
-//                            return console.error('error running query', err);
-//                        }
-//                        next();
-//                        client.end();
-//                    });
-//                });
-//            } else {
-//                //console.log('Старый пользователь');
-//                client.query("SELECT id, ip, time, num, (extract(epoch from now()) - time) as delta from visits WHERE ip='" + req.ip + "'", function(err, result) {
-//                    if(err) {
-//                        return console.error('error running query', err);
-//                    }
-//                    var timeSinceLastVisit = result.rows[0].delta;
-//                    var ipId = result.rows[0]['id'];
-//                    var ip = result.rows[0]['ip'];
-//                    var countOfVisits = parseInt(result.rows[0].num, 10);
-//                    //console.log(ipId, timeSinceLastVisit, countOfVisits);
-//                    // Обновим время последнего посещения
-//                    client.query("UPDATE visits SET time = extract(epoch from now()) WHERE id = " + ipId, function(err, result) {
-//                        if(err) {
-//                            return console.error('error running query', err);
-//                        }
-//                    });
-//                    // 30 минут неактивности - новый визит
-//                    if (timeSinceLastVisit >= 180000) {
-//                        client.query("UPDATE visits SET num =" + (countOfVisits + 1) +  "WHERE id = " + ipId, function(err, result) {
-//                            if(err) {
-//                                return console.error('error running query', err);
-//                            }
-//                            // Запишем еще время этого визита
-//                            client.query("INSERT INTO visits_time (ip, time) VALUES ($1::text, CURRENT_DATE)", [ip], function(err, result) {
-//                                if(err) {
-//                                    return console.error('error running query', err);
-//                                }
-//                                client.end();
-//                            });
-//                        });
-//                    }
-//                    // Слишком быстро запросы идут
-//                    // Поставим timeout
-//                    // Проверим, что это не ajax запрос
-//                    var is_ajax_request = req.xhr;
-//                    var isRedirect = req.app.locals.isRedirected || false;
-//                    console.log('isRedirect', isRedirect);
-//                    console.log('isXHR', is_ajax_request);
-//                    if (timeSinceLastVisit <= 0.5 && !isRedirect && !is_ajax_request) {
-//                        console.log('Too fast');
-//                        res.render('hacker', {visits: allVisits});
-//                        client.end();
-//                        return;
-//                    }
-//                    req.app.locals.isRedirected = false;
-//                    next();
-//                });
-//            }
-//        });
-//    });
-//});
 
 module.exports = router;
