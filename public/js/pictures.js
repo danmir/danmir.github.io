da3625ac1a948046095089b0e80fe87663ebbc22
danmir <danmir@me.com>
2015-12-19 00:16:25 +0500

    Links

--- public/js/pictures.js
+++ public/js/pictures.js
@@ -1,47 +1,47 @@
 window.pictures = [
     {
         name: 'pic1',
-        previewPic: 'img/cat1-preview.jpg',
-        fullPic: 'img/cat1.jpg'
+        previewPic: '/img/cat1-preview.jpg',
+        fullPic: '/img/cat1.jpg'
     },
     {
         name: 'pic2',
-        previewPic: 'img/cat2-preview.jpg',
-        fullPic: 'img/cat2.jpg'
+        previewPic: '/img/cat2-preview.jpg',
+        fullPic: '/img/cat2.jpg'
     },
     {
         name: 'pic3',
-        previewPic: 'img/cat3-preview.jpg',
-        fullPic: 'img/cat3.jpg'
+        previewPic: '/img/cat3-preview.jpg',
+        fullPic: '/img/cat3.jpg'
     },
     {
         name: 'pic4',
-        previewPic: 'img/cat4-preview.jpg',
-        fullPic: 'img/cat4.jpg'
+        previewPic: '/img/cat4-preview.jpg',
+        fullPic: '/img/cat4.jpg'
     },
     {
         name: 'pic5',
-        previewPic: 'img/cat5-preview.jpg',
-        fullPic: 'img/cat5.jpg'
+        previewPic: '/img/cat5-preview.jpg',
+        fullPic: '/img/cat5.jpg'
     },
     {
         name: 'pic6',
-        previewPic: 'img/cat6-preview.jpg',
-        fullPic: 'img/cat6.jpg'
+        previewPic: '/img/cat6-preview.jpg',
+        fullPic: '/img/cat6.jpg'
     },
     {
         name: 'pic7',
-        previewPic: 'img/cat7-preview.jpg',
-        fullPic: 'img/cat7.jpg'
+        previewPic: '/img/cat7-preview.jpg',
+        fullPic: '/img/cat7.jpg'
     },
     {
         name: 'pic8',
-        previewPic: 'img/cat8-preview.jpg',
-        fullPic: 'img/cat8.jpg'
+        previewPic: '/img/cat8-preview.jpg',
+        fullPic: '/img/cat8.jpg'
     },
     {
         name: 'pic9',
-        previewPic: 'img/cat9-preview.jpg',
-        fullPic: 'img/cat9.jpg'
+        previewPic: '/img/cat9-preview.jpg',
+        fullPic: '/img/cat9.jpg'
     }
 ];
